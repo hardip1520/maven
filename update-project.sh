22c5ab916c27761a8044235c0b0369b5cd3f1906
Hardipsinh <hardipsinhmatroja1520@gmail.com>
2023-07-16 21:07:39 -0400

    adding update-project.sh

---
+++ update-project.sh
@@ -0,0 +1,16 @@
+git pull
+
+echo "building images"
+docker build -t hardip1520/jenkins-sample .
+
+echo "docker login"
+
+docker login --userame=hardip1520 --pasword=Hardip@1520
+
+echo "pushing image"
+
+docker push hardip1520/jenkins-sample
+
+echo "running container"
+
+docker run -p 9090:8080 -d hardip1520/jenkins-sample
